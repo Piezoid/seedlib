00892cce520282a479e0d49685b68dc3671c7980
Maël Kerbiriou <piezo.w.dimd@gmail.com>
2019-11-27 14:53:50 +0000

    add lis

--- src/seedlib.cpp
+++ src/seedlib.cpp
@@ -77,6 +77,83 @@ index::stat(memreport_t& report, const std::string& prefix) const
     pimpl->stat(report, prefix);
 }
 
+template<typename ValueT = uint32_t, typename SizeT = ValueT> struct LIS
+{
+  public:
+    using value_type = ValueT;
+    using size_type  = SizeT;
+
+  protected:
+    struct entry
+    {
+        value_type value;
+        size_type  pred;
+    };
+    std::vector<entry>     X;
+    std::vector<size_type> M;
+
+  public:
+    void init(size_type len = 0)
+    {
+        X.clear();
+        M.clear();
+        X.reserve(len);
+        M.reserve(len);
+    }
+
+    size_t seq_size() const { return X.size(); }
+
+    size_t subseq_size() const { return M.size(); }
+
+    value_type operator[](size_t i) const { return X[i].value; }
+
+    // Add a value to the lis
+    void push_back(value_type x)
+    {
+        auto      i = value_type(X.size()); // Indice of the added element
+        size_type pred;                     // Predecessor for the subsequence ending at the added element
+
+        if (i > 0) { // Not the first element
+            // If we are continuing the current sub-sequence
+            const size_type subseq_end = M.back();
+            if (x >= X[subseq_end].value) {
+                pred = subseq_end;
+                M.push_back(i);
+            } else {
+                // Binary search for the longuest subsequence of length subseq_len ≤ best_len that ends with a value
+                // X[M[subseq_len_m1]].value ≤ x
+                size_t subseq_len_m1 = 0;
+                size_t n             = M.size() - 1;
+                while (n > 1) {
+                    size_t half   = n >> 1u;
+                    size_t mid    = subseq_len_m1 + half;
+                    subseq_len_m1 = (X[M[mid]].value <= x) ? mid : subseq_len_m1;
+                    n -= half;
+                }
+                subseq_len_m1    = size_t(X[M[subseq_len_m1]].value <= x) + subseq_len_m1;
+                pred             = subseq_len_m1 >= 1 ? M[subseq_len_m1 - 1] : 0;
+                M[subseq_len_m1] = i;
+            }
+        } else {
+            pred = 0;
+            M.push_back(0);
+        }
+        X.push_back({x, pred});
+    }
+
+    // Returns the indices in the original sequence of the LIS elements
+    const std::vector<size_type>& traceback()
+    {
+        // Reconstruct the longest increasing subsequence
+        const auto last = M.rend() - 1;
+        for (auto it = M.rbegin(); it != last;) {
+            size_type pred = X[*it].pred;
+            *++it          = pred;
+        }
+        return M;
+    }
+};
+
 class query::impl
   : public gatbl::details::sequence2kmers_base<query::impl, gatbl::kmer_window<seed_types::kmerins_t>>
   , public seed_model<>
@@ -91,6 +168,8 @@ class query::impl
     const callback_t             on_read;
 
     std::vector<seed_t>    results;
+    std::vector<seed_t>    filtered_results;
+    LIS<query_pos_t>       lis;
     std::vector<mapping_t> mappings;
     read_id_t              curr_read = read_id_t(-1);
 
@@ -139,6 +218,9 @@ class query::impl
         const auto  query_len = read_pos_t(this->get_next_pos());
 
         std::sort(results.begin(), results.end());
+        filtered_results.clear();
+        filtered_results.reserve(results.size()); // Ensure that it will not reallocate while we are appending
+        auto filtered_results_begin = &*filtered_results.begin();
 
         const auto last = results.end();
         for (auto it = results.begin(); it < last;) {
@@ -147,14 +229,33 @@ class query::impl
             assume(target_bounds.first <= target_bounds.second && it->target_pos >= target_bounds.first
                      && it->target_pos < target_bounds.second,
                    "invalid position interval");
-            const seed_t* first         = &(*it);
-            const auto    target_length = read_pos_t(target_bounds.second - target_bounds.first);
+
+            lis.init(query_len);
+            const seed_t* unfiltered_seeds_start = &(*it);
+            const auto    target_length          = read_pos_t(target_bounds.second - target_bounds.first);
             for (; it < last && it->target_pos < target_bounds.second; it++) {
                 assume(it->target_pos >= target_bounds.first, "invalid position interval");
-                it->target_pos -= target_bounds.first;
+                lis.push_back(it->query_pos);
             }
 
-            mappings.emplace_back(mapping_t{target_id, target_length, first, &(*it)});
+            const auto& traceback = lis.traceback();
+            if (traceback.size() >= 5) {
+                const seed_t* filtered_seeds_start = &*filtered_results.end();
+                auto          prev_seed            = seed_t{std::numeric_limits<query_pos_t>::min(), 0};
+                for (auto idx : traceback) {
+                    const auto& seed = unfiltered_seeds_start[idx];
+                    assume(seed.target_pos >= target_bounds.first && seed.target_pos < target_bounds.second,
+                           "seed outside of current target read");
+                    assert(seed.target_pos >= prev_seed.target_pos && seed.query_pos >= prev_seed.query_pos,
+                           "not increasing sequence");
+                    filtered_results.emplace_back(seed_t{seed.query_pos, seed.target_pos - target_bounds.first});
+                    prev_seed = seed;
+                }
+
+                assert(filtered_results_begin == &*filtered_results.begin(), "filtered_results reallocated");
+                mappings.emplace_back(
+                  mapping_t{target_id, target_length, filtered_seeds_start, &*filtered_results.end()});
+            }
         }
 
         bool do_next_read = on_read(curr_read, query_len, mappings);
